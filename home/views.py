8264bdf854cec1a908accacb51544bff33c73f18
KC Shafer <kshafer@2u.com>
2014-05-31 14:34:33 -0400

    Home index using bootstrap and home app

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader, RequestContext
+
+def index(request):
+    template = loader.get_template('home/index.html')
+    context = RequestContext(request)
+    return HttpResponse(template.render(context))
--- liwi/settings.py
+++ liwi/settings.py
@@ -86,6 +86,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 AUTH_USER_MODEL = 'registration.User'
