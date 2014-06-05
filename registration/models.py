2bdd5454b39a31b5e260e30482d69fa5ca805725
KC Shafer <kshafer@2u.com>
2014-06-05 18:32:38 -0400

    New field on user to store whether they are an artist

--- registration/models.py
+++ registration/models.py
@@ -2,4 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    is_artist = models.BooleanField()
     type = models.CharField(max_length=80)
