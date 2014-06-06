25fb623f9db914ceaba521aa1c828bc95496be3e
KC Shafer <kshafer@2u.com>
2014-06-06 17:52:54 -0400

    Lots of front end cleanup, including moving the signup to the right of the nav if not auth, or a user drop down if auth

--- user_profile/models.py
+++ user_profile/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.ForeignKey('registration.User')
-    bio = models.TextField()
-    twitter = models.CharField(max_length=80)
+    bio = models.TextField(blank=True, null=True)
+    twitter = models.CharField(max_length=80, blank=True, null=True)
 
     class Meta:
         db_table = 'user_profile'
--- user_profile/urls.py
+++ user_profile/urls.py
@@ -4,7 +4,7 @@ from user_profile import views
 
 urlpatterns = patterns('',
     url(r'^myprofile/edit', views.edit_profile, name='edit_profile'),
-    url(r'^view/([0-9])/$', views.view_profile, name='view_profile'),
+    url(r'^view/([0-9]+)/$', views.view_profile, name='view_profile'),
     url(r'^saveprofile/', views.save_profile, name='save_profile')
     )
 
