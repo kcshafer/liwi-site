9d92f18132c72888307b006c5f6f348079ed5225
KC Shafer <kshafer@2u.com>
2014-06-06 16:46:47 -0400

    Places artists art in photos/art/user_id

--- art/models.py
+++ art/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+def upload_to(instance, filename):
+    return 'art/%s/%s' % (instance.user.id, filename)
+
 class Art(models.Model):
     user = models.ForeignKey('registration.User', blank=True)
-    photo = models.ImageField(upload_to='photos/')
+    photo = models.ImageField(upload_to=upload_to)
     title = models.CharField(max_length=80)
     description = models.TextField()
 
