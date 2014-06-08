b65e2e6b606f156b9edd545ae8d76a7b18a9fc23
KC Shafer <kshafer@2u.com>
2014-06-08 11:44:04 -0400

    Display image on mass artist views

--- artists/views.py
+++ artists/views.py
@@ -7,7 +7,7 @@ from django.template import loader, RequestContext
 from registration.models import User
 
 def index(request):
-    artists = User.objects.all().filter(is_artist=True)[:10]
+    artists = User.objects.raw('select registration_user.id, registration_user.username, user_profile.photo from registration_user inner join user_profile on registration_user.id=user_profile.user_id')    
     template = loader.get_template('artists/artists_view.html')
     context = RequestContext(request, {'artists': artists})
 
