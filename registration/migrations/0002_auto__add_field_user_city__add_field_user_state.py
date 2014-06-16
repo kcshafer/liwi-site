68f3306f734c3f43cda69dd91162b554c85aa5c6
KC Shafer <kshafer@2u.com>
2014-06-16 18:05:06 -0400

    Had to change the user table name from registration_user to user, it was driving me crazy

---
+++ registration/migrations/0002_auto__add_field_user_city__add_field_user_state.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        db.rename_table('registration_user', 'user')
+        # Adding field 'User.city'
+        db.add_column('user', 'city',
+                      self.gf('django.db.models.fields.CharField')(default='N/A', max_length=80),
+                      keep_default=False)
+
+        # Adding field 'User.state'
+        db.add_column('user', 'state',
+                      self.gf('django.db.models.fields.CharField')(default='N/A', max_length=80),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'User.city'
+        db.delete_column('user', 'city')
+
+        # Deleting field 'User.state'
+        db.delete_column('user', 'state')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'registration.securityanswer': {
+            'Meta': {'object_name': 'SecurityAnswer', 'db_table': "'secret_answer'"},
+            'answer': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'security_questions': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['registration.SecurityQuestion']"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['registration.User']"})
+        },
+        u'registration.securityquestion': {
+            'Meta': {'object_name': 'SecurityQuestion', 'db_table': "'secret_question'"},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'question': ('django.db.models.fields.CharField', [], {'max_length': '250'})
+        },
+        u'registration.user': {
+            'Meta': {'object_name': 'User', 'db_table': "'user'"},
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_artist': ('django.db.models.fields.BooleanField', [], {}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'state': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        }
+    }
+
+    complete_apps = ['registration']
--- registration/models.py
+++ registration/models.py
@@ -4,6 +4,11 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     is_artist = models.BooleanField()
     type = models.CharField(max_length=80)
+    city = models.CharField(max_length=80)
+    state = models.CharField(max_length=80)
+
+    class Meta:
+        db_table = 'user'
 
 class SecurityQuestion(models.Model):
     question = models.CharField(max_length = 250, null=False)
