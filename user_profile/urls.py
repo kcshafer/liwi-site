7e2e26834aefe66bb58d8f9b77b299a196f91091
KC Shafer <kshafer@2u.com>
2014-06-18 18:30:38 -0400

    Just getting this out of the unstaged finally

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -5,6 +5,6 @@ from user_profile import views
 urlpatterns = patterns('',
     url(r'^myprofile/edit', views.edit_profile, name='edit_profile'),
     url(r'^view/([0-9]+)/$', views.view_profile, name='view_profile'),
-    url(r'^saveprofile/', views.save_profile, name='save_profile')
+    url(r'^saveprofile/', views.save_profile, name='save_profile'),
     )
 
