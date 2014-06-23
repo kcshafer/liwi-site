736c564c05b1beab374b8a9c5c2105ab990b7066
KC Shafer <kshafer@2u.com>
2014-06-23 11:46:49 -0400

    Fixes #40

--- artists/views.py
+++ artists/views.py
@@ -4,7 +4,7 @@ from django.template import loader, RequestContext
 from registration.models import User
 
 def index(request):
-    artists = User.objects.raw('select registration_user.id, registration_user.username, user_profile.photo from registration_user inner join user_profile on registration_user.id=user_profile.user_id')    
+    artists = User.objects.raw('select registration_user.id, registration_user.username, user_profile.photo from registration_user inner join user_profile on registration_user.id=user_profile.user_id where is_active=True and is_artist=True')
     template = loader.get_template('artists/artists_view.html')
     context = RequestContext(request, {'artists': artists})
 
